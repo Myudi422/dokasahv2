69db43ee783d6d65c36370be23865743e6c98fe9
Hzu <43848105+Myudi422@users.noreply.github.com>
2025-03-12 03:05:29 +0000

    xa

---
+++ next-sitemap.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://dokasah.web.id', // Ganti dengan domain website kamu
+    generateRobotsTxt: true, // Opsional, untuk buat robots.txt
+  };
+  
